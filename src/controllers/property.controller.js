16dca97ea852286525b9f9b204dd24ca35c091e8
Delight-Fela-Steve <dfelastevegit@gmail.com>
2022-05-20 13:31:16 +0100

    added the createProperty controller

--- src/controllers/property.controller.js
+++ src/controllers/property.controller.js
@@ -1,6 +1,6 @@
 const Property = require('../models/properties.models');
 
-exports.create=(req,res)=>{
+exports.createProperty=(req,res)=>{
     const {owner, status, price, state, city, address, type, image_url} = req.body;
     const property = new Property(parseInt(owner.trim()),status.trim(),price.trim(),state.trim(),city.trim(),address.trim(), type.trim(), image_url.trim())
     Property.create(property,(err, data)=>{
