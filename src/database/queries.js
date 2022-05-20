ff53bb7ee2831bbc2a2f82cd8da725336e390f19
Delight-Fela-Steve <dfelastevegit@gmail.com>
2022-05-20 10:02:16 +0100

    added to user model and added user db functions

--- src/database/queries.js
+++ src/database/queries.js
@@ -6,6 +6,25 @@ const createDB = `CREATE DATABASE IF NOT EXISTS ${DB_NAME}`;
 
 const dropDB = `DROP DATABASE IF EXISTS ${DB_NAME}`;
 
+const createTableUsers = `
+CREATE TABLE IF NOT EXISTS users (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    firstname VARCHAR(50) NULL,
+    lastname VARCHAR(50) NULL,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    password VARCHAR(255) NOT NULL,
+    created_on TIMESTAMP(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6)
+)
+`;
+
+const createNewUser = `
+INSERT INTO users VALUES(null, ?, ?, ?, ?, NOW())
+`;
+
+const findUserByEmail = `
+SELECT * FROM users WHERE email = ?
+`;
+
 const createTableProperties = `
 CREATE TABLE IF NOT EXISTS properties (
     id INT PRIMARY KEY AUTO_INCREMENT,
@@ -35,6 +54,9 @@ const updatePropertyStatus = `UPDATE properties SET status = ? WHERE id = ?`;
 module.exports = {
     createDB,
     dropDB,
+    createTableUsers,
+    createNewUser,
+    findUserByEmail,
     createTableProperties,
     createNewProperty,
     findPropertyByType,
---
+++ src/models/user.models.js
@@ -0,0 +1,51 @@
+const db = require('../config/db.config');
+const { createNewUser, findUserByEmail } = require('../database/queries');
+const { logger } = require('../utils/logger');
+
+class User {
+    constructor(firstname, lastname, email, password) {
+        this.firstname = firstname;
+        this.lastname = lastname;
+        this.email = email;
+        this.password = password;
+    }
+
+    static create(newUser, cb) {
+        db.query(createNewUser, 
+            [
+                newUser.firstname, 
+                newUser.lastname, 
+                newUser.email, 
+                newUser.password
+            ], (err, res) => {
+                if (err) {
+                    logger.error(err.message);
+                    cb(err, null);
+                    return;
+                }
+                cb(null, {
+                    id: res.insertId,
+                    firstname: newUser.firstname,
+                    lastname: newUser.lastname,
+                    email: newUser.email
+                });
+        });
+    }
+
+    static findByEmail(email, cb) {
+        db.query(findUserByEmail, email, (err, res) => {
+            if (err) {
+                logger.error(err.message);
+                cb(err, null);
+                return;
+            }
+            if (res.length) {
+                cb(null, res[0]);
+                return;
+            }
+            cb({ kind: "not_found" }, null);
+        })
+    }
+}
+
+module.exports = User;
